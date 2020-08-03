ac37eda6a7a13845be3c6b0713de0c659fcd0b3e
lrd9426464 <675848920@qq.com>
2020-08-03 11:19:38 +0800

    添加购物车

--- demo/miniprogram/pages/product_detail/product_detail.js
+++ demo/miniprogram/pages/product_detail/product_detail.js
@@ -1,5 +1,6 @@
 // pages/product_detail/product_detail.js
 const db=wx.cloud.database();
+const _=db.command;
 
 Page({
 
@@ -7,14 +8,84 @@ Page({
    * 页面的初始数据
    */
   data: {
-    product_src:"",
+    product_src:[],
     product_name:"",
     product_num:"",
     product_price:"",
     product_detail:"",
-    product_xq_src:""
+    product_xq_src:"",
+    id:""
   },
 
+  /* 加入购物车 */
+  into_shopping_cart(){
+    db.collection("shopping_cart").where({
+      product_id:this.data.id
+    }).get({
+      success:res=>{
+        console.log(res);
+        if(res.data==""){
+          db.collection("shopping_cart").add({
+            data:{
+              product_src:this.data.product_src[0],
+              product_name:this.data.product_name,
+              product_price:this.data.product_price,
+              product_num:1,
+              product_id:this.data.id,
+            },success:res=>{
+              console.log('商品添加购物车成功',res);
+              wx.showToast({
+                title: '加入成功',
+              })
+            },fail:res=>{
+              console.log('商品添加购物车失败',res);
+            }
+          })
+        }else{
+          wx.showToast({
+            title: '已经有这个商品',
+            icon:'none'
+          })
+        }
+      },fail:res=>{
+        console.log(res)
+      }
+    })
+  },
+
+  buy(){
+    db.collection("shopping_cart").where({
+      product_id:this.data.id
+    }).get({
+      success:res=>{
+        console.log(res);
+        if(res.data==""){
+          db.collection("shopping_cart").add({
+            data:{
+              product_src:this.data.product_src[0],
+              product_name:this.data.product_name,
+              product_price:this.data.product_price,
+              product_num:1,
+              product_id:this.data.id,
+            },success:res=>{
+              console.log('商品添加购物车成功',res);
+              wx.switchTab({
+                url: '../shopping_cart/shopping_cart',
+              })
+            },fail:res=>{
+              console.log('商品添加购物车失败',res);
+            }
+          })
+        }else{
+          wx.switchTab({
+            url: '../shopping_cart/shopping_cart',
+          })
+        }
+      },fail:res=>{
+        console.log(res)
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -30,6 +101,7 @@ Page({
           product_price:res.data.price,
           product_detail:res.data.detail,
           product_xq_src:res.data.product_xq_src,
+          id:res.data._id
         })
       },fail:res=>{
         console.log('商品详细信息获取失败',res)
